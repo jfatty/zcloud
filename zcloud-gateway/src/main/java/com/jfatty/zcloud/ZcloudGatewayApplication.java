b3045e12ba50d1ff579ff107023037e9dc97a640
admin <zealsoft#yeah.net>
2019-11-14 17:34:55 +0800

    init zcloud

--- zcloud-gateway/src/main/java/com/jfatty/zcloud/ZcloudGatewayApplication.java
+++ zcloud-gateway/src/main/java/com/jfatty/zcloud/ZcloudGatewayApplication.java
@@ -1,13 +1,22 @@
 package com.jfatty.zcloud;
 
+import com.jfatty.zcloud.gateway.RequestTimeGatewayFilterFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZcloudGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZcloudGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RequestTimeGatewayFilterFactory requestTimeGatewayFilterFactory() {
+        return new RequestTimeGatewayFilterFactory();
     }
 
 }
---
+++ zcloud-gateway/src/main/java/com/jfatty/zcloud/gateway/RequestTimeGatewayFilterFactory.java
@@ -0,0 +1,73 @@
+package com.jfatty.zcloud.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 描述
+ *
+ * @author jfatty on 2019/11/14
+ * @email jfatty@163.com
+ */
+@Slf4j
+public class RequestTimeGatewayFilterFactory  extends AbstractGatewayFilterFactory<RequestTimeGatewayFilterFactory.Config> {
+
+
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+    private static final String KEY = "withParams";
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList(KEY);
+    }
+
+    public RequestTimeGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+            return chain.filter(exchange).then(
+                    Mono.fromRunnable(() -> {
+                        Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                        if (startTime != null) {
+                            StringBuilder sb = new StringBuilder(exchange.getRequest().getURI().getRawPath())
+                                    .append(": ")
+                                    .append(System.currentTimeMillis() - startTime)
+                                    .append("ms");
+                            if (config.isWithParams()) {
+                                sb.append(" params:").append(exchange.getRequest().getQueryParams());
+                            }
+                            log.warn(sb.toString());
+                        }
+                    })
+            );
+        };
+    }
+
+
+
+
+
+
+    public static class Config {
+
+        private boolean withParams;
+
+        public boolean isWithParams() {
+            return withParams;
+        }
+
+        public void setWithParams(boolean withParams) {
+            this.withParams = withParams;
+        }
+
+    }
+}
