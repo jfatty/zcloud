1967095b04a8301633c08ac7e5a07a3b900d3ca6
admin <zealsoft#yeah.net>
2019-12-10 17:52:23 +0800

    init zcloud

---
+++ zcloud-system/zcloud-system-common/src/main/java/com/jfatty/zcloud/system/interfaces/IPermRelationship.java
@@ -0,0 +1,17 @@
+package com.jfatty.zcloud.system.interfaces;
+
+import com.jfatty.zcloud.base.interfaces.BInterface;
+import com.jfatty.zcloud.system.entity.PermRelationship;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 描述
+ *
+ * @author jfatty on 2019/12/4
+ * @email jfatty@163.com
+ */
+@RequestMapping(value={"/perm"})
+public interface IPermRelationship extends BInterface<PermRelationship> {
+
+
+}
