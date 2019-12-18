f7d24c92da57685b5bb338d30010d26019376dbe
admin <zealsoft#yeah.net>
2019-12-18 19:11:56 +0800

    init zcloud

---
+++ zcloud-system/zcloud-system-service/src/main/java/com/jfatty/zcloud/system/api/ApiCusIdentityFileController.java
@@ -0,0 +1,63 @@
+package com.jfatty.zcloud.system.api;
+
+import com.jfatty.zcloud.base.utils.RELResultUtils;
+import com.jfatty.zcloud.base.utils.ResultUtils;
+import com.jfatty.zcloud.system.entity.IdentityFile;
+import com.jfatty.zcloud.system.req.IdentityFileReq;
+import com.jfatty.zcloud.system.res.IdentityFileRes;
+import com.jfatty.zcloud.system.service.IdentityFileService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 描述
+ *
+ * @author jfatty on 2019/12/16
+ * @email jfatty@163.com
+ */
+@Slf4j
+@RestController
+@RequestMapping(value={"/api/cusIdentityFile"})
+public class ApiCusIdentityFileController {
+
+
+    @Autowired
+    private IdentityFileService identityFileService ;
+
+
+    @ApiOperation(value="获取评估量表或者调查问卷表格数据",notes="获取评估量表或者调查问卷表格数据",  position = 0 )
+    @RequestMapping(value={"/table/list"},method = RequestMethod.GET )
+    RELResultUtils<IdentityFileRes> table(@RequestParam(value = "v" , defaultValue = "20191101") String v ,
+                                          @RequestParam(value = "pageIndex" , defaultValue = "1" ) Integer pageIndex ,
+                                          @RequestParam(value = "pageSize" , defaultValue = "10") Integer pageSize) {
+        RELResultUtils<IdentityFile> ss = identityFileService.getTable("",pageIndex,pageSize);
+        RELResultUtils<IdentityFileRes> res = new RELResultUtils<IdentityFileRes>();
+        BeanUtils.copyProperties(ss,res);
+        return res ;
+    }
+
+    @ApiOperation(value = "添加项目", notes = "添加项目管理", nickname = "liukx")
+    @RequestMapping(value={"/save"},method=RequestMethod.POST)
+    ResultUtils save(@RequestBody IdentityFileReq entity) {
+        IdentityFile identityFile = new IdentityFile();
+        try {
+            identityFile.setAvatar(entity.getAvatar().getBytes());
+            identityFile.setEmblem(entity.getEmblem().getBytes());
+            if(identityFileService.save(identityFile))
+                return ResultUtils.build(200, "SUCCESS") ;
+            return ResultUtils.build(500, "FAILED") ;
+        } catch ( Exception e ) {
+            log.error( "保存异常 异常信息为 : [{}]" , e.getMessage() );
+        }
+        return ResultUtils.build(500, "网络异常请稍后重试") ;
+    }
+
+
+}
---
+++ zcloud-system/zcloud-system-service/src/main/java/com/jfatty/zcloud/system/config/SwaggerConfiguration.java
@@ -0,0 +1,51 @@
+package com.jfatty.zcloud.system.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableSwaggerBootstrapUi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 描述
+ *
+ * @author jfatty on 2019/12/15
+ * @email jfatty@163.com
+ */
+
+@Configuration
+@EnableSwagger2
+@EnableSwaggerBootstrapUi
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.jfatty.zcloud.system.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("平台接口")
+                .description("API接口")
+                .termsOfServiceUrl("https://www.jfatty.com/")
+                .contact(new Contact("jfatty ", "https://www.jfatty.com/", "jfatty@163.com"))
+                .version("1.1")
+                .build();
+    }
+
+
+}
