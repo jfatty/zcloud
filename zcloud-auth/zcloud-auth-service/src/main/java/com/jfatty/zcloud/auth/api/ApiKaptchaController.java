528c3a04e859f2490f4b08fe1f7af872949e58ed
admin <zealsoft#yeah.net>
2019-12-20 00:52:26 +0800

    init zcloud

--- zcloud-auth/zcloud-auth-service/src/main/java/com/jfatty/zcloud/auth/api/ApiKaptchaController.java
+++ zcloud-auth/zcloud-auth-service/src/main/java/com/jfatty/zcloud/auth/api/ApiKaptchaController.java
@@ -27,12 +27,15 @@ public class ApiKaptchaController {
     @ApiOperation(value="001******向用户手机号发送验证码")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "phone", value = "时间戳",dataType = "String",defaultValue = "13177261541"),
-            @ApiImplicitParam(name = "random", value = "时间戳/随机字符串",dataType = "String",defaultValue = "122151881")
+            @ApiImplicitParam(name = "random", value = "时间戳/随机字符串",dataType = "String",defaultValue = "122151881"),
+            @ApiImplicitParam(name = "appId", value = "每个应用都对应有appId支付宝、微信、第三方APP",dataType = "String",defaultValue = "wx4712402349f957a4")
     })
     @RequestMapping(value = {"/kaptcha"} ,method = RequestMethod.GET)
     public RETResultUtils<String> kaptcha(@RequestParam(value = "phone" , defaultValue = "1321541582") String phone ,//
                                           @RequestParam(value = "random" , defaultValue = "122151881" ) String random,//
+                                          @RequestParam(value = "appId" , defaultValue = "wx4712402349f957a4" ) String appId,//
                                           HttpServletRequest request ){
+        System.out.println(appId);
         System.out.println(random);
         System.out.println(phone);
         return new RETResultUtils("验证码发送成功,请注意查收") ;
