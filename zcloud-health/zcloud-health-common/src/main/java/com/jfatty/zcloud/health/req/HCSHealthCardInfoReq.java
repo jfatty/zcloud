0f7037e96d217682af92570fdf93189c6eaa882c
admin <zealsoft#yeah.net>
2019-12-27 22:26:18 +0800

    init zcloud

--- zcloud-health/zcloud-health-common/src/main/java/com/jfatty/zcloud/health/req/HCSHealthCardInfoReq.java
+++ zcloud-health/zcloud-health-common/src/main/java/com/jfatty/zcloud/health/req/HCSHealthCardInfoReq.java
@@ -16,27 +16,27 @@ import lombok.Data;
 public class HCSHealthCardInfoReq extends HCSHealthCardInfoDTO<HCSHealthCardInfoReq> {
 
 
-    @ApiModelProperty(name = "birthday", position = 0, value = "出生年月日",required = true ,example = "1996-01-23")
+    @ApiModelProperty(name = "birthday", position = 0, value = "出生年月日",required = true ,example = "1995-08-13")
     private String birthday ;
-    @ApiModelProperty(name = "idType", position = 0, value = "证件类型",required = true ,example = "01-居民身份证")
+    @ApiModelProperty(name = "idType", position = 0, value = "证件类型01-居民身份证",required = true ,example = "01")
     private String idType ;
-    @ApiModelProperty(name = "address", position = 0, value = "地址",required = true ,example = "广东省深圳市腾讯大厦")
+    @ApiModelProperty(name = "address", position = 0, value = "地址",required = true ,example = "湖北省仙桃市通海口镇石垸村三组31号")
     private String address;
     @ApiModelProperty(name = "gender", position = 0, value = "性别",required = true ,example = "男")
     private String gender;
     @ApiModelProperty(name = "nation", position = 0, value = "民族",required = true ,example = "汉族")
     private String nation;
-    @ApiModelProperty(name = "name", position = 0, value = "姓名",required = true ,example = "小邓")
+    @ApiModelProperty(name = "name", position = 0, value = "姓名",required = true ,example = "李志康")
     private String name;
     //@ApiModelProperty(name = "wechatCode", position = 0, value = "微信身份码",required = true ,example = "73EFA6796D3869FF82FAE7E81E9XXXX")
     //private String wechatCode;
-    @ApiModelProperty(name = "idNumber", position = 0, value = "证件号码",required = true ,example = "432624198888883116")
+    @ApiModelProperty(name = "idNumber", position = 0, value = "证件号码",required = true ,example = "429004199508134038")
     private String idNumber;
-    @ApiModelProperty(name = "phone1", position = 0, value = "联系方式1",required = true ,example = "18565818888")
+    @ApiModelProperty(name = "phone1", position = 0, value = "联系方式1",required = true ,example = "18062158054")
     private String phone1;
     @ApiModelProperty(name = "phone2", position = 0, value = "联系方式2",required = true ,example = "18565818899")
     private String phone2;
-    @ApiModelProperty(name = "patid", position = 0, value = "院内ID",required = true ,example = "1003243")
+    @ApiModelProperty(name = "patid", position = 0, value = "院内ID",required = true ,example = "100112")
     private String patid;
 
 }
--- zcloud-health/zcloud-health-common/src/main/java/com/jfatty/zcloud/health/res/RegHealthCardInfoRes.java
+++ zcloud-health/zcloud-health-common/src/main/java/com/jfatty/zcloud/health/res/RegHealthCardInfoRes.java
@@ -36,8 +36,8 @@ public class RegHealthCardInfoRes extends HCSHealthCardInfoDTO<RegHealthCardInfo
     /**
      * 扩展字段
      */
-    @ApiModelProperty(name = "adminExt", position = 0, value = "扩展字段",example = "{\"qr_code\":\"C220AE414CE6EE581037C311AE24518FCFE19C429BECD478C1A13976260FXXXX:1\",\"ecardId\":\"C220AE414CE6EE581037C311AE24518FCFE19C429BECD478C1A13976260FXXXX\",\"main_index\":\"584AF4B1D110B8BB7CBAC978C03B657191DF84863144E436B5CA38C0F0E2XXXX\",\"id_type\":\"居民身份证\",\"id_number\":\"身份证号码\",\"name\":\"姓名\",\"sex\":\"性别\",\"birthday\":\\\"1998-09-08\",\"telephone\":\"手机号码\",\"nation\":\"民族\",\"unit\":\"null\",\"address\":\"地址\"}")
-    private String adminExt;
+    //@ApiModelProperty(name = "adminExt", position = 0, value = "扩展字段",example = "{\"qr_code\":\"C220AE414CE6EE581037C311AE24518FCFE19C429BECD478C1A13976260FXXXX:1\",\"ecardId\":\"C220AE414CE6EE581037C311AE24518FCFE19C429BECD478C1A13976260FXXXX\",\"main_index\":\"584AF4B1D110B8BB7CBAC978C03B657191DF84863144E436B5CA38C0F0E2XXXX\",\"id_type\":\"居民身份证\",\"id_number\":\"身份证号码\",\"name\":\"姓名\",\"sex\":\"性别\",\"birthday\":\\\"1998-09-08\",\"telephone\":\"手机号码\",\"nation\":\"民族\",\"unit\":\"null\",\"address\":\"地址\"}")
+    //private String adminExt;
 
 
 
