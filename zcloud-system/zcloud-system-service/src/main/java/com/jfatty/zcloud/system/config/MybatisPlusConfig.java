4a1b8f3d803c9f894e85ef0b55446549e2050c77
admin <zealsoft#yeah.net>
2019-12-30 09:23:39 +0800

    init zcloud

--- zcloud-system/zcloud-system-service/src/main/java/com/jfatty/zcloud/system/config/MybatisPlusConfig.java
+++ zcloud-system/zcloud-system-service/src/main/java/com/jfatty/zcloud/system/config/MybatisPlusConfig.java
@@ -36,7 +36,12 @@ public class MybatisPlusConfig extends BaseConfig {
     @Bean
     @Profile({"dev","local"})// 设置 dev local 环境开启
     public PerformanceInterceptor performanceInterceptor(){
-        return super.performanceInterceptor();
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        /*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*/
+        performanceInterceptor.setMaxTime(180000);
+        /*<!--SQL是否格式化 默认false-->*/
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
     }
 
 }
