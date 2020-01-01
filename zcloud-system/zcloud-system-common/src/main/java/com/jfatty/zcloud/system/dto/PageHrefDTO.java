abbec1491b0657f1364fcd3b100e6186d0a57865
admin <zealsoft#yeah.net>
2020-01-01 16:10:48 +0800

    init zcloud

--- zcloud-system/zcloud-system-common/src/main/java/com/jfatty/zcloud/system/dto/PageHrefDTO.java
+++ zcloud-system/zcloud-system-common/src/main/java/com/jfatty/zcloud/system/dto/PageHrefDTO.java
@@ -3,6 +3,7 @@ package com.jfatty.zcloud.system.dto;
 import com.jfatty.zcloud.base.dto.BaseDTO;
 import lombok.Data;
 
+
 /**
  * 描述
  *
@@ -11,5 +12,52 @@ import lombok.Data;
  */
 @Data
 public class PageHrefDTO<T extends BaseDTO> extends BaseDTO {
+
+    /**
+     * 主键ID
+     */
+    private String id;
+
+    /**
+     * 应用ID APPID
+     */
+    private String appid;
+
+    /**
+     * 页面标识ID
+     */
+    private String pageId;
+
+    /**
+     * 页面标签ID 或者是唯一确定元素
+     */
+    private String elemId;
+
+    /**
+     * 图片标题
+     */
+    private String elemTitle;
+
+    /**
+     * 链接显示类型 按钮或者a标签
+     */
+    private String elemType;
+
+    /**
+     * 目标地址
+     */
+    private String targetHref;
+
+    /**
+     * 设置字段 根据需求使用本字段
+     */
+    private String site;
+
+    /**
+     * 备注或者描述
+     */
+    private String description;
+
+
 
 }
--- zcloud-system/zcloud-system-common/src/main/java/com/jfatty/zcloud/system/res/PageHrefRes.java
+++ zcloud-system/zcloud-system-common/src/main/java/com/jfatty/zcloud/system/res/PageHrefRes.java
@@ -1,8 +1,12 @@
 package com.jfatty.zcloud.system.res;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jfatty.zcloud.system.dto.PageHrefDTO;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 /**
  * 描述
@@ -13,4 +17,22 @@ import lombok.Data;
 @Data
 @ApiModel(description = "界面链接跳转开发配置响应实体")
 public class PageHrefRes extends PageHrefDTO<PageHrefRes> {
+
+    /**
+     * 域值
+     */
+    private String realm;
+
+    /**
+     * 使用状态
+     */
+    private Integer state;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
 }
