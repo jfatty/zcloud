8ada6a0c56203c50c3d49c970a9f26c58fd1300f
admin <zealsoft#yeah.net>
2020-01-06 08:01:40 +0800

    init zcloud

--- zcloud-hospital/zcloud-hospital-service/src/main/java/com/jfatty/zcloud/hospital/api/ApiPayNotifyController.java
+++ zcloud-hospital/zcloud-hospital-service/src/main/java/com/jfatty/zcloud/hospital/api/ApiPayNotifyController.java
@@ -50,7 +50,7 @@ public class ApiPayNotifyController {
     @Autowired
     private AlipayConfigService alipayConfigService ;
 
-    @Autowired
+    //@Autowired
     //private WechatFeignClient wechatFeignClient ;
 
     /**
