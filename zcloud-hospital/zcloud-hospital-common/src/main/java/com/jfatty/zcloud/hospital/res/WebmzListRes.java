b221b2dae77935f602cf5a82f4d54fcb81975618
admin <zealsoft#yeah.net>
2020-01-09 15:51:13 +0800

    init zcloud

--- zcloud-hospital/zcloud-hospital-common/src/main/java/com/jfatty/zcloud/hospital/res/WebmzListRes.java
+++ zcloud-hospital/zcloud-hospital-common/src/main/java/com/jfatty/zcloud/hospital/res/WebmzListRes.java
@@ -19,10 +19,12 @@ import java.util.List;
 @ApiModel(description = "就诊人待缴费单信息包含列表响应实体")
 public class WebmzListRes extends UnPayOutpatientDTO<WebmzListRes> {
 
-    @ApiModelProperty(name = "name", position = 2,required = true, value = "姓名 " , example = "2")
+    @ApiModelProperty(name = "name", position = 2,required = true, value = "患者姓名 " , example = "2")
     private String name ;
     @ApiModelProperty(name = "idCard", position = 2,required = true, value = "身份证号 " , example = "422801125158582571747")
     private String idCard ;
+    @ApiModelProperty(name = "jzh", position = 2,required = true, value = "就诊号 " , example = "202001080574")
+    private String jzh ;
 
     private List<UnPayOutpatientRes> unPays ;
 
