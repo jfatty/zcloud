ef99abfc512a62f42b48923b816a8b4b3785854f
admin <zealsoft#yeah.net>
2020-01-13 15:46:30 +0800

    init zcloud

--- zcloud-health/zcloud-health-service/src/main/java/com/jfatty/zcloud/health/api/ApiHealthCardStationViewController.java
+++ zcloud-health/zcloud-health-service/src/main/java/com/jfatty/zcloud/health/api/ApiHealthCardStationViewController.java
@@ -130,7 +130,7 @@ public class ApiHealthCardStationViewController {
                 String params = getPostParams(hcsHealthCardInfoRes);
                 params = URLEncoder.encode(params,"UTF-8");
                 log.error("编码后的URL参数[{}]",params);
-                path = settings.getDetailUrl() + "?" + params ;
+                path = String.format(settings.getDetailUrl(),hospitalId) + "?" + params ;
                 //去健康卡详情页面
                 log.error("去健康卡详情页面 [{}]",path);
             }
