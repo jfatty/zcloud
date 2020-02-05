957eef56d44b14a629a79df66f755680f4551ce2
admin <zealsoft#yeah.net>
2020-02-05 15:32:16 +0800

    init zcloud

--- zcloud-gateway/src/main/java/com/jfatty/zcloud/ZcloudGatewayApplication.java
+++ zcloud-gateway/src/main/java/com/jfatty/zcloud/ZcloudGatewayApplication.java
@@ -3,11 +3,13 @@ package com.jfatty.zcloud;
 import com.jfatty.zcloud.gateway.RequestBodyRoutePredicateFactory;
 import com.jfatty.zcloud.gateway.RequestTimeGatewayFilterFactory;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+@MapperScan({"com.jfatty.zcloud.mapper"})
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
