db8c3c3d35439bcdfd83108829c0efcde602ede6
admin <zealsoft#yeah.net>
2020-04-20 20:14:04 +0800

    init zcloud

--- zcloud-gateway/src/main/java/com/jfatty/zcloud/ZcloudGatewayApplication.java
+++ zcloud-gateway/src/main/java/com/jfatty/zcloud/ZcloudGatewayApplication.java
@@ -3,12 +3,14 @@ package com.jfatty.zcloud;
 import com.jfatty.zcloud.gateway.RequestBodyRoutePredicateFactory;
 import com.jfatty.zcloud.gateway.RequestTimeGatewayFilterFactory;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @MapperScan({"com.jfatty.zcloud.mapper"})
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,6 +19,7 @@ public class ZcloudGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZcloudGatewayApplication.class, args);
+        log.error("ZcloudGatewayApplication 启动成功!");
     }
 
     @Bean
